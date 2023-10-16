2eaf47df3bcffb569b0a53b24e97aa3d0fcf5164
stove-jeebs <1876595827qq@gmail.com>
2023-10-17 11:32:01 +1300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,18 @@
-#![allow(dead_code)]
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     io::stdin,
 };
 
-pub mod graph {
-    use super::*;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Vertex(pub usize);
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct Vertex(pub usize);
-
-    #[derive(Debug)]
-    pub struct GraphAdjList {
-        pub adj_list: HashMap<Vertex, Vec<Vertex>>,
-    }
-
-    impl GraphAdjList {
-        pub fn new(edges: Vec<[Vertex; 2]>) -> Self {
-            let mut graph = GraphAdjList {
-                adj_list: HashMap::new(),
-            };
-
-            // populate graph
-            for edge in edges {
-                graph.add_vertex(edge[0]);
-                graph.add_vertex(edge[1]);
-                graph.add_edge(edge[0], edge[1]);
-            }
-
-            graph
-        }
-
-        pub fn add_edge(&mut self, vet1: Vertex, vet2: Vertex) {
-            // add edge or panic if entry doesn't exists
-            self.adj_list
-                .entry(vet1)
-                .and_modify(|vec| vec.push(vet2))
-                .or_insert_with(|| panic!("Failed to add edge: Entry {vet1:?} not found"));
-            self.adj_list
-                .entry(vet2)
-                .and_modify(|vec| vec.push(vet1))
-                .or_insert_with(|| panic!("Failed to add edge: Entry {vet2:?} not found"));
-        }
-
-        pub fn add_vertex(&mut self, vet: Vertex) {
-            // add vertex if it doesn't exist
-            self.adj_list.entry(vet).or_insert(vec![]);
-        }
-    }
+#[derive(Debug)]
+pub struct GraphAdjList {
+    pub adj_list: HashMap<Vertex, Vec<Vertex>>,
 }
 
 pub mod girth {
     use super::*;
-    use graph::{GraphAdjList, Vertex};
 
     fn vet_cycle_len_bfs(graph: &mut GraphAdjList, start_vet: Vertex) -> usize {
         // return the cycle length for start_vet
@@ -101,48 +61,48 @@ pub mod girth {
     }
 }
 
-fn main() {
-    use graph::{GraphAdjList, Vertex};
+fn read_graph() -> Option<(GraphAdjList, usize)> {
+    let mut num_nodes = String::new();
+    stdin()
+        .read_line(&mut num_nodes)
+        .expect("Failed to read line");
+    let num_nodes: usize = num_nodes
+        .trim()
+        .parse()
+        .expect("Failed to parse num_nodes into i32");
 
-    let mut idx = 1;
-    loop {
-        let mut num_nodes = String::new();
-        stdin()
-            .read_line(&mut num_nodes)
-            .expect("Failed to read line");
-        let num_nodes: usize = num_nodes
+    if num_nodes == 0 {
+        return None;
+    }
+
+    let mut graph = GraphAdjList {
+        adj_list: HashMap::new(),
+    };
+    for i in 0..num_nodes {
+        let mut line = String::new();
+        stdin().read_line(&mut line).expect("Failed to read line");
+        let line: Vec<Vertex> = line
             .trim()
-            .parse()
-            .expect("Failed to parse num_nodes into i32");
+            .split_whitespace()
+            .map(|num_str| {
+                Vertex(
+                    num_str
+                        .parse::<usize>()
+                        .expect("Failed to parse into a usize"),
+                )
+            })
+            .collect();
+        graph.adj_list.entry(Vertex(i)).or_insert(line);
+    }
 
-        if num_nodes == 0 {
-            break;
-        }
+    Some((graph, num_nodes))
+}
 
-        let mut graph = GraphAdjList {
-            adj_list: HashMap::new(),
-        };
-        for i in 0..num_nodes {
-            let mut line = String::new();
-            stdin().read_line(&mut line).expect("Failed to read line");
-            // turn a string of numbers separated by spaces into Vec<Vertex(usize)>
-            let line: Vec<Vertex> = line
-                .trim()
-                .split_whitespace()
-                .map(|num_str| {
-                    Vertex(
-                        num_str
-                            .parse::<usize>()
-                            .expect("Failed to parse into a usize"),
-                    )
-                })
-                .collect();
-            // insert line ito adjacency list if entry doesn't exist
-            graph.adj_list.entry(Vertex(i)).or_insert(line);
-        }
-
+fn main() {
+    let mut idx = 1;
+    while let Some((mut graph, num_nodes)) = read_graph() {
         let g = girth::compute_girth(&mut graph, num_nodes);
-        println!("Graph {idx} has girth {g}.");
+        println!("Graph {} has girth {}.", idx, g);
         idx += 1;
     }
 }
